dd8ed3638cc3428a2ebcf19e0b45e291cb074187
birhanu <107028439+hruybirhanu@users.noreply.github.com>
2022-11-17 19:54:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    </Routes>
+    </>
+  );
+}
+export default App;
+                  
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+   <h2>well come to home pages</h2>
+    </div>
+  )
+}
